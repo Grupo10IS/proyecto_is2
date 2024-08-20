a48598c1c589a4d5a51fc3743f29eab930191ed3
Elias Gill <eliasgill42@gmail.com>
2024-08-20 18:17:33 -0400

    feat: generacion automatica de codigo
    La generacion automatica de codigo se realiza con Sphinx.
    Ademas se tiene una github action que genera esta documentacion y genera
    una nueva "github page" con el html.

---
+++ conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+from datetime import date
+
+import django
+
+# Add the path to your Django project to sys.path
+sys.path.insert(0, os.path.abspath("."))
+
+# Set the Django settings module
+os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings"
+django.setup()
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Proyecto IS2 grupo 6"
+copyright = f"{date.today().year}, Grupo 6 IS2"
+author = "Grupo 6 IS2"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",  # anotaciones de tipo
+]
+
+templates_path = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+language = "es"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = []
