c2ed0b5e50a26296e9aded4c3b6f341b355a2030
Elias Gill <eliasgill42@gmail.com>
2024-08-27 13:56:22 -0400

    test: Anadido test para asegurarse de que el usuario admin tenga permisos de acceso.

--- modulos/UserProfile/test_views.py
+++ modulos/UserProfile/test_views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from modulos.UserProfile.management.commands.new_admin import (_credentials,
+                                                               create_admin)
 from modulos.UserProfile.models import UserProfile
 
 
@@ -45,11 +47,29 @@ def test_login_view(client):
 
 @pytest.mark.django_db
 def test_user_list_view(client, django_user_model):
-    # Crear un usuario de prueba
+    # Crear un usuario de prueba sin permisos
+
     admin_user = django_user_model.objects.create_user(
         username="admin", password="adminpassword", email="admin@example.com"
     )
     client.login(username="admin", password="adminpassword")
+
+    url = reverse("user_list")
+    response = client.get(url)
+    assert response.status_code == 403
+
+    # Crear un usuario de prueba con permisos
+
+    create_admin(
+        _credentials(
+            username="true_admin",
+            paswd="password",
+            pas_conf="password",
+            email="adimns@gmail.com",
+        )
+    )
+
+    client.login(username="true_admin", password="password")
 
     url = reverse("user_list")
     response = client.get(url)
