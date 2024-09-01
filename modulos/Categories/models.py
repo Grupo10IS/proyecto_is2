78c920605e85d43969c70a013488bdaa1fdfdeb0
Diaz-Abel <diazabel743@gmail.com>
2024-08-31 23:52:20 -0400

    feat(models): agregado modelo de Categoria, con atributos básicos

--- modulos/Categories/models.py
+++ modulos/Categories/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Category():
+    name = models.CharField(max_length=80, verbose_name="Nombre", blank=True)
+    description = models.TextField(blank=True, null=True, verbose_name="Descrición")
