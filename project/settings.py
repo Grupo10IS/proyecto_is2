fb4aa4a68981f3e7e375f8cfe1bf6ad6d827d2ac
Juancho Gonza <72740630+JuanchoGonza98@users.noreply.github.com>
2024-09-02 01:24:22 -0400

    Update settings.py para entorno Dev y Prod
    Esto es necesario para poder cargar los statics en el directorio modulos y que se pueda usar. Leer en la wiki el procedimiento para tomar estos cambios cada vez que se agreguen nuevos archivos estaticos

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'modulos'),]
 COMPRESS_ENABLED = True
 COMPRESS_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_FINDERS = [
