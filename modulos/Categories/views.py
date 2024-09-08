d69dee3db1a745f614cc816563ad3c6951cdb452
Ariel Vallejos <arielito.vallejo@fpuna.edu.py>
2024-09-08 12:01:34 -0400

    feat: listamos los contenidos por categorias, cambios en el front end

--- modulos/Categories/views.py
+++ modulos/Categories/views.py
@@ -42,8 +42,12 @@ class CategoryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Agregar más contexto
-        return new_ctx(self.request, context)
+        # Obtener los posts relacionados a esta categoría
+        category = self.get_object()
+        context["posts"] = Post.objects.filter(
+            category=category
+        )  # Filtrar los posts por categoría
+        return context
 
 
 # Vista para crear categorias
