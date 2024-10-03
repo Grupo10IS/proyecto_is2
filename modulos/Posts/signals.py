bc4b0ca8f36a0f7ef11294b9ee6a42c1e33b531b
Ariel Vallejos <arielito.vallejo@fpuna.edu.py>
2024-10-02 22:26:32 -0400

    notificacion a usuarios de nuevos contenidos, notificacion a autores de post publicado o rechazado

--- modulos/Posts/signals.py
+++ modulos/Posts/signals.py
@@ -4,9 +4,8 @@ from django.dispatch import receiver
 
 from modulos.Posts.models import Log, Post
 from modulos.UserProfile.models import UserProfile
-
 from .models import Post
-
+from django.db.models.signals import pre_save
 
 @receiver(post_save, sender=Post)
 def log_post_change(sender, instance, **kwargs):
@@ -37,41 +36,120 @@ def log_post_change(sender, instance, **kwargs):
         Log(post=instance, message=changes).save()
 
 
-@receiver(post_save, sender=Post)
-def send_notification_to_users(sender, instance, created, **kwargs):
-    if created:
-        # solo notificar los posts que sean publicados, no los borradores y demas
-        if instance.status != Post.PUBLISHED:
-            return
+@receiver(pre_save, sender=Post)
+def send_notification_to_users(sender, instance, **kwargs):
+    """
+    Envía una notificación a los usuarios cuando un post cambia a estado "Publicado".
+    """
+    if instance.pk:
+        # Obtenemos el estado anterior del post para verificar si el estado ha cambiado a "PUBLISHED"
+        old_instance = Post.objects.filter(pk=instance.pk).first()
 
-        category = instance.category
+        if (
+            old_instance
+            and old_instance.status != Post.PUBLISHED
+            and instance.status == Post.PUBLISHED
+        ):
 
-        # Filtrar usuarios que desean recibir notificaciones y tienen acceso a la categoría
-        recipients = UserProfile.objects.filter(
-            receive_notifications=True,  # Usuarios que desean recibir notificaciones
-        ).distinct()
+            category = instance.category
 
-        # Filtrar usuarios que tienen acceso a la categoría del post
-        if category.tipo == category.PREMIUM:
-            recipients = recipients.filter(
-                payment__category=category,
-                payment__status="completed",  # Solo pagos completados
-            ).distinct()
-        elif category.tipo == category.SUSCRIPCION:
-            recipients = recipients.filter(
-                is_staff=False,  # Filtrar usuarios no administradores
-                is_superuser=False,  # Filtrar superusuarios
+            recipients = UserProfile.objects.filter(
+                receive_notifications=True,
             ).distinct()
 
-        # Enviar un correo personalizado a cada destinatario
-        for recipient in recipients:
-            subject = f"Nueva publicación en la categoría {category.name}"
-            message = f"Hola {recipient.username} 👋👋, hay una nueva publicación en la categoría {category.name}. No te la pierdas!, http://localhost:8000/posts/{instance.id}"
+            if category.tipo == category.PREMIUM:
 
-            send_mail(
-                subject,
-                message,
-                "groupmakex@gmail.com",  # Dirección de remitente
-                [recipient.email],  # Enviar a cada usuario individualmente
-                fail_silently=False,
-            )
+                recipients = recipients.filter(
+                    payment__category=category,
+                    payment__status="completed",
+                ).distinct()
+            elif category.tipo == category.SUSCRIPCION:
+
+                recipients = recipients.filter(
+                    is_staff=False,
+                    is_superuser=False,
+                ).distinct()
+
+            for recipient in recipients:
+
+                subject = f"Nueva publicación en la categoría {category.name}💯💥"
+                message = f"Hola {recipient.username}👋👋, hay una nueva publicación en la categoría {category.name}. No te la pierdas!, http://localhost:8000/posts/{instance.id}"
+
+                send_mail(
+                    subject,
+                    message,
+                    "groupmakex@gmail.com",
+                    [recipient.email],
+                    fail_silently=False,
+                )
+
+
+@receiver(pre_save, sender=Post)
+def send_notification_to_authors(sender, instance, **kwargs):
+    """
+    Envía una notificación al autor cuando su post cambia de estado a "Publicado" o es "Rechazado".
+    """
+    if instance.pk:
+        # Obtenemos el estado anterior del post para verificar los cambios
+        old_instance = Post.objects.filter(pk=instance.pk).first()
+
+        if old_instance:
+            print(f"Signal ejecutado para el post: {instance.title}")
+            print(f"Estado anterior: {old_instance.status}")
+            print(f"Estado actual: {instance.status}")
+
+            # Verificamos si hay un autor asignado al post
+            author = instance.author
+            if not author:
+                print(
+                    f"No se encontró un autor para el post {instance.title}. No se enviará notificación."
+                )
+                return
+
+            print(f"Autor del post: {author.username}")
+            print(f"Email del autor: {author.email}")
+
+            # Notificar al autor si el estado cambió a "Publicado"
+            if (
+                old_instance.status != Post.PUBLISHED
+                and instance.status == Post.PUBLISHED
+            ):
+                print(
+                    f"El post {instance.title} ha sido publicado. Notificando al autor {author.username}."
+                )
+                subject = "Tu post ha sido publicado 🎉"
+                message = f"Hola {author.username}, tu post '{instance.title}' ha sido publicado exitosamente. ¡Gracias por tu contribución!"
+                send_mail(
+                    subject,
+                    message,
+                    "groupmakex@gmail.com",
+                    [author.email],
+                    fail_silently=False,
+                )
+                print(
+                    f"Correo enviado al autor {author.username} notificando la publicación."
+                )
+
+            # Notificar al autor si el post ha sido "Rechazado" (cambio a "Borrador")
+            elif (
+                old_instance.status in [Post.PENDING_REVIEW, Post.PENDING_PUBLICATION]
+                and instance.status == Post.DRAFT
+            ):
+                print(
+                    f"El post {instance.title} ha sido rechazado. Notificando al autor {author.username}."
+                )
+                subject = "Tu post ha sido rechazado 😕"
+                message = (
+                    f"Hola {author.username}, tu post '{instance.title}' ha sido rechazado y vuelto al estado de borrador. "
+                    "Por favor, revisa los comentarios o haz los ajustes necesarios antes de volver a enviarlo para revisión."
+                )
+                send_mail(
+                    subject,
+                    message,
+                    "groupmakex@gmail.com",
+                    [author.email],
+                    fail_silently=False,
+                )
+                print(
+                    f"Correo enviado al autor {author.username} notificando el rechazo."
+                )
