94b64c6c8127b0f09103e7a53adf905d42988582
EliasGill <79729891+Elias-Gill@users.noreply.github.com>
2024-10-15 20:10:15 -0300

    refactor: moving statics to embrace django standars (#37)
    * refactor: moving statics to embrace django standars
    
    * fix: converting favicon.png to favicon.ico
    
    * fix: adding favicon to base.html

--- project/settings.py
+++ project/settings.py
@@ -1,17 +1,12 @@
 # Cargar la configuración específica de producción si DJANGO_ENV está seteada a "production".
-import os
 from os import getenv
 
 # Cargar configuraciones compartidas entre dev y producción
 from .envs.common import *
 
-print(f"DJANGO_ENV is: {os.getenv('DJANGO_ENV')}")
-
 if getenv("DJANGO_ENV") == "production":
+    print(f"DJANGO_ENV: Produccion")
     from .envs.prod import *
-
-    print("Modo Producción")
 else:
+    print(f"DJANGO_ENV: Development")
     from .envs.dev import *
-
-    print("Modo Desarrollo")
