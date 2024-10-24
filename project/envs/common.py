840257b3ddba709a6d39d8efac86aa935e2a29ff
juanchogonza98 <jhonandres90@hotmail.com>
2024-10-24 10:24:16 -0300

    FIX: Se arreglan los test de la app Post editando una linea para referirse a los templates en el env. common

--- project/envs/common.py
+++ project/envs/common.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
