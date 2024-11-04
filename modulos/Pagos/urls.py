f0179a304f91af3bdd001f9ab962ae1f2f486a10
Elias Gill <eliasgill42@gmail.com>
2024-11-04 17:55:06 -0300

    refactor: renaming url paths to be more consistent

--- modulos/Pagos/urls.py
+++ modulos/Pagos/urls.py
@@ -1,27 +1,25 @@
 from django.urls import path
-from .views import (
-    financial_view,
-    payment_success,
-    payment_view,
-    purchased_categories_view,
-    user_payment_view,
-    export_payments_excel,
-    export_user_payments_excel,
-)
+
+from .views import (export_payments_excel, export_user_payments_excel,
+                    financial_view, payment_success, payment_view,
+                    purchased_categories_view, user_payment_view)
 
 urlpatterns = [
-    path("pay/<int:category_id>/", payment_view, name="payment_view"),
-    path("success/<int:category_id>/", payment_success, name="payment_success"),
+    # vistas
+    path("", user_payment_view, name="user_payment_view"),
     path(
-        "mis-categorias-premium/",
+        "purchased/",
         purchased_categories_view,
         name="purchased_categories",
     ),
-    path("panel/", financial_view, name="financial_view"),
-    path("mis-pagos/", user_payment_view, name="user_payment_view"),
-    path("export/excel/", export_payments_excel, name="export_payments_excel"),
+    path("admin/", financial_view, name="financial_view"),
+    # crud
+    path("pay/<int:category_id>/", payment_view, name="payment_view"),
+    path("success/<int:category_id>/", payment_success, name="payment_success"),
+    # exportacion de reportes
+    path("admin/export/", export_payments_excel, name="export_payments_excel"),
     path(
-        "export_user_payments_excel/",
+        "export",
         export_user_payments_excel,
         name="export_user_payments_excel",
     ),
